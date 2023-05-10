2d1ba30148e2c6337443c1080806a5cc32345abc
Fuviathan <FuyuSora02@gmail.com>
2023-05-10 23:35:42 +0700

    change path

--- treefamily/Frontend/pages/addmember.js
+++
@@ -1,9 +0,0 @@
-import UpdateMember from "../components/FunctionMember/UpdateMember";
-import AddMember from "../components/FunctionMember/AddMember";
-import BaseTemplate from "../components/BaseTemplate";
-import FamilyTreChart from "../components/FamilyTreeChart";
-import Modal from "../components/UI/Modal";
-export default function addmember() {
-  // return <AddMember></AddMember>;
-  return <Modal></Modal>;
-}
