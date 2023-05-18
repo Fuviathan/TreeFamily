6504d02cf0b88361f7461302df69af0afcec4302
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-18 23:15:33 +0700

    fix link and change page's name

--- treefamily/Frontend/pages/displayTypeTree.js
+++ treefamily/Frontend/pages/displayTypeTree.js
@@ -9,7 +9,7 @@ export default function displayTypeTree() {
         type="button"
         className="fixed z-20 inline-flex items-center justify-center px-4 py-2 ml-12 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm top-12 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
       >
-        <a href="/userpage">Xem dạng bảng</a>
+        <a href="/home">Xem dạng bảng</a>
       </button>
       <FamilyTreChart></FamilyTreChart>
     </BaseTemplate>
---
+++ treefamily/Frontend/pages/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainPage from '../components/UserPage/MainPage'
+
+export default function UserPage() {
+  return (
+    <MainPage />
+  )
+}
