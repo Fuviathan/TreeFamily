f68a754ad5f8989133cedf9d5e8f3414534e1d52
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-18 23:14:26 +0700

    delete profile's image

--- treefamily/Frontend/components/Sidebar/Profile.js
+++ treefamily/Frontend/components/Sidebar/Profile.js
@@ -5,14 +5,7 @@ export default function Profile() {
   return (
     <div className="flex flex-shrink-0 p-4 bg-gray-700">
       <Link href="/test" className="flex-shrink-0 block w-full group">
-        <div className="flex items-center">
-          <div>
-            <img
-              className="inline-block rounded-full h-9 w-9"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </div>
+        <div className="flex items-center"> 
           <div className="ml-3">
             <p className="text-sm font-medium text-white">Tom Cook</p>
             <p className="text-xs font-medium text-gray-300 group-hover:text-gray-200">
