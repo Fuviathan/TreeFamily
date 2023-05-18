5c028e35e7b683a1ea073e5835e92bd828c8ee0b
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-18 23:48:34 +0700

    change button to vietnamese

--- treefamily/Frontend/components/UserPage/MainPage.js
+++ treefamily/Frontend/components/UserPage/MainPage.js
@@ -36,7 +36,7 @@ export default function MainPage() {
                   type="button"
                   className="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                 >
-                  Add user
+                  Thêm thành viên
                 </button>
               </div>
             </div>
