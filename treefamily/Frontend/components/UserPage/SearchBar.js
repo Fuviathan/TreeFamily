396a38e02a9dcfc498568190f98b4a4539ae13b3
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-18 15:58:53 +0700

    fix searchbar

--- treefamily/Frontend/components/UserPage/SearchBar.js
+++ treefamily/Frontend/components/UserPage/SearchBar.js
@@ -9,7 +9,7 @@ export default function SearchBar() {
         placeholder="Search"
       ></input>
 
-      <button type="submit" class="absolute right-0 top-0 mr-1">
+      <button type="submit" className="absolute right-0 top-0 mr-1">
         <MagnifyingGlassCircleIcon className="w-auto h-10 text-gray-400 rounded-lg hover:text-gray-600"></MagnifyingGlassCircleIcon>
       </button>
     </div>
