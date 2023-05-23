3410db67bf5b479d54b372b39a985bdbac90cc06
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-23 23:08:35 +0700

    finished detail user

---
+++ treefamily/Frontend/components/DetailPage/DetailCard.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import InputLine from '../UI/InputLine'
+import useSWR from 'swr'
+
+export default function DetailCard({ person }) {
+    const findUserById = (Id) => {
+        if (Id === null || Id === "" || Id === 0) return null
+        else {
+            const { data: user, error } = useSWR(`http://localhost:8080/member/get-by-id-member?id=${Id}`)
+            if (!user) return null
+            return user?.fullName
+        }
+    }
+    return (
+        <div className="grid grid-cols-1 px-6 mt-10 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <InputLine
+                {...{
+                    data: person?.fullName,
+                    className: "sm:col-span-3",
+                    title: "Họ Tên",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.gender,
+                    className: "sm:col-span-1",
+                    title: "Giới tính",
+                }}
+            ></InputLine>
+            <InputLine
+                {...{
+                    data: person?.role,
+                    className: "sm:col-span-2",
+                    title: "Vai trò",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.dateOfBirth,
+                    className: "sm:col-span-3",
+                    title: "Ngày sinh",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.mobilePhoneNumber,
+                    className: "sm:col-span-3",
+                    title: "Số điện thoại",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.career,
+                    className: "sm:col-span-3",
+                    title: "Nghề nghiệp",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.education,
+                    className: "sm:col-span-3",
+                    title: "Bằng cấp",
+                }}
+            ></InputLine>
+
+            <div className="sm:col-span-4"></div>
+
+            <InputLine
+                {...{
+                    data: findUserById(person?.dadId),
+                    className: "sm:col-span-3",
+                    title: "Họ tên cha",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: findUserById(person?.momId),
+                    className: "sm:col-span-3",
+                    title: "Họ tên mẹ",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.maritalStatus,
+                    className: "sm:col-span-3",
+                    title: "Tình trạng hôn nhân",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: findUserById(person?.partnerId),
+                    className: "sm:col-span-3",
+                    title: "Họ tên vợ/chồng",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.status === "Đã mất" ? "Đã mất" : "",
+                    className: "sm:col-span-3",
+                    title: "Trạng thái",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.dateOfDeath,
+                    className: "sm:col-span-3",
+                    title: "Ngày mất",
+                }}
+            ></InputLine>
+
+            <InputLine
+                {...{
+                    data: person?.burialPlace,
+                    className: "sm:col-span-6",
+                    title: "Nơi an táng",
+                }}
+            ></InputLine>
+        </div>
+    )
+}
--- treefamily/Frontend/pages/detailUser.js
+++ treefamily/Frontend/pages/detailUser.js
@@ -1,38 +1,22 @@
 import React from "react";
 import BaseTemplate from "../components/BaseTemplate";
-import { For } from "react-haiku";
-import DetailItem from "../components/DetailPage/DetailItem";
+import { useSession } from "next-auth/react";
+import DetailCard from "../components/DetailPage/DetailCard";
 
-export default function test() {
+export default function detailUser() {
+  const { data: userData, error } = useSession();
   return (
     <BaseTemplate>
-      <div className="h-full px-6 py-4 bg-white">
+      <div className="h-full px-6 py-4 bg-gray-200">
         <div>
           <h3 className="text-xl font-bold leading-6 text-gray-900 uppercase">
             Thông tin cá nhân
           </h3>
         </div>
-        <div className='mt-4 overflow-scroll bg-gray-300 rounded-lg shadow-lg h-[90vh]'>
-          <div className="px-2 border-t border-gray-200">
-            <dl className="sm:divide-y sm:divide-gray-200">
-              <DetailItem dt={"Họ tên"} dd={"Margot Foster"} />
-              <DetailItem dt={"Giới tính"} dd={"Margot Foster"} />
-              <DetailItem dt={"Ngày Sinh"} dd={"Margot Foster"} />
-              <DetailItem dt={"Số ĐT"} dd={"Margot Foster"} />
-              <DetailItem dt={"Nghề nghiệp"} dd={"Margot Foster"} />
-              <DetailItem dt={"Học vấn"} dd={"Margot Foster"} />
-              <DetailItem dt={"Họ tên cha"} dd={"Margot Foster"} />
-              <DetailItem dt={"Họ tên mẹ"} dd={"Margot Foster"} />
-              <DetailItem dt={"Tình trạng hôn nhân"} dd={"Margot Foster"} />
-              <DetailItem dt={"Tên vợ/chồng"} dd={"Margot Foster"} />
-              <DetailItem dt={"Vai trò"} dd={"Margot Foster"} />
-              <DetailItem dt={"Trạng thái"} dd={"Margot Foster"} />
-              <DetailItem dt={"Nơi an táng"} dd={"Margot Foster"} />
-              <DetailItem dt={"Ngày mất"} dd={"Margot Foster"} />
-            </dl>
-          </div>
+        <div className='inset-0 mt-8 overflow-auto bg-white rounded-lg shadow-lg h-85vh'>
+          <DetailCard person={userData?.user} />
         </div>
       </div>
-    </BaseTemplate>
+    </BaseTemplate >
   );
 }
