838c878ecba4b518a08c7452b4ea4a0630490598
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-05-26 00:43:35 +0700

    delete

--- treefamily/Frontend/components/UI/Modal.js
+++
@@ -1,8 +0,0 @@
-import { Fragment } from "react";
-// import ReactDOM from "react-dom";
-// import classes from "./Modal.module.css";
-
-const Modal = (props) => {
-  return <div className="bg-red-500 h-screen w-full">11111111</div>;
-};
-export default Modal;
