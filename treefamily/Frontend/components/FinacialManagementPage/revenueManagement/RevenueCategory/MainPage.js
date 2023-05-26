c987463c9345da8ff53a5b771a7a20d0e3d8a07f
nga <=>
2023-05-26 15:41:04 +0700

    add main page of category

--- treefamily/Frontend/components/FinacialManagementPage/revenueManagement/RevenueCategory/MainPage.js
+++ treefamily/Frontend/components/FinacialManagementPage/revenueManagement/RevenueCategory/MainPage.js
@@ -2,43 +2,53 @@ import BaseTemplate from "../../../BaseTemplate";
 import { If } from "react-haiku";
 import { useState } from "react";
 import TableOfASingleCategory from "./TableOfASingleCategory";
+import useSWR from "swr";
+import AddItemInCategory from './Modal/AddItemInCategory'
 
-export default function MainPage({ id }) {
-    // const [addNewRevenue, setAddNewRevenue] = useState(false)
-    return (
-        <>
-            <BaseTemplate>
-                <div className="h-full py-4 bg-white">
-                    {/* <div className="mx-6 text-xl font-bold uppercase">Decripstion</div> */}
-                    <div className="px-4 sm:px-6 lg:px-8">
-                        <div className="sm:flex sm:items-center">
-                            <div className="flex sm:flex-auto">
-                                <h1 className="mt-1 text-xl font-semibold text-gray-900">
-                                    Danh sách các khoản thu
-                                </h1>
+export default function MainPage({ pid }) {
+  const { data, error } = useSWR(
+    "http://localhost:8080/revenue-management/get-all"
+  );
+  const miniData = data.filter((object) => {
+    return (object.id = { pid });
+  });
+  const year = miniData[0]?.year;
+  const [addNewRevenue, setAddNewRevenue] = useState(false);
+  return (
+    <>
+      <BaseTemplate>
+        <div className="h-full py-4 bg-white">
+          {/* <div className="mx-6 text-xl font-bold uppercase">Decripstion</div> */}
+          <div className="px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center">
+              <div className="flex sm:flex-auto">
+                <h1 className="mt-1 text-xl font-semibold text-gray-900">
+                  Danh sách các khoản thu năm {year}
+                </h1>
+              </div>
 
-                            </div>
-
-                            <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                                <button
-                                    onClick={() => setAddNewRevenue(true)}
-                                    type="button"
-                                    className="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                                >
-                                    Thêm khoản thu
-                                </button>
-                            </div>
-                        </div>
-                        <TableOfASingleCategory id={id}/>
-                    </div>
-                </div>
-                {/* <If isTrue={addNewRevenue}>
-                    <AddRevenue
-                        onClose={() => setAddNewRevenue(false)}
-                        isVisible={addNewRevenue}
-                    />
-                </If> */}
-            </BaseTemplate>
-        </>
-    );
+              <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                <button
+                  onClick={() => setAddNewRevenue(true)}
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                >
+                  Thêm khoản thu
+                </button>
+              </div>
+            </div>
+            <TableOfASingleCategory pid={pid} />
+          </div>
+        </div>
+        <If isTrue={addNewRevenue}>
+          <AddItemInCategory
+            onClose={() => setAddNewRevenue(false)}
+            isVisible={addNewRevenue}
+            year={year}
+            id={pid}
+          />
+        </If>
+      </BaseTemplate>
+    </>
+  );
 }
