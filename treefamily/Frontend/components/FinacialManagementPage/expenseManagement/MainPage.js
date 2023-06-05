d75b615cfb1a8511560c4ad1f2434c15ccfd02db
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-06-05 17:00:58 +0700

    some expense statistics

--- treefamily/Frontend/components/FinacialManagementPage/expenseManagement/MainPage.js
+++ treefamily/Frontend/components/FinacialManagementPage/expenseManagement/MainPage.js
@@ -19,8 +19,16 @@ export default function MainPage() {
                   Danh sách các khoản chi
                 </h1>
               </div>
-
               <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                <Link
+                  href="/financialManagement/revenueManagement/revenueStatistics"
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                >
+                  Báo cáo chi
+                </Link>
+              </div>
+              <div className="mt-4 sm:mt-0 sm:ml-8 sm:flex-none">
                 <button
                   onClick={() => setAddNewSponsor(true)}
                   type="button"
---
+++ treefamily/Frontend/components/FinacialManagementPage/expenseManagement/Statistics/ItemExpenseStatistics.js
@@ -0,0 +1,23 @@
+import React from "react";
+export default function ItemExpenseStatistics({ item, index }) {
+  return (
+    <tr>
+      <td className="py-4 pr-10 text-sm font-medium text-left text-gray-900 whitespace-nowrap sm:pl-6 ">
+        {index + 1}
+      </td>
+      <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+        {item.revenueName}
+      </td>
+      <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+        {item.revenuePerPerson}
+      </td>
+
+      <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+        {item.dueDate}
+      </td>
+      <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+        {item.status ? "Đã đóng" : "Chưa đóng"}
+      </td>
+    </tr>
+  );
+}
