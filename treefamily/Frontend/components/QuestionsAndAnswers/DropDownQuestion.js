3cf69e6aa76e965064c39970de7104ea9a47370d
Fu <84614670+Fuviathan@users.noreply.github.com>
2023-07-10 01:39:24 +0700

    question almost done

---
+++ treefamily/Frontend/components/QuestionsAndAnswers/DropDownQuestion.js
@@ -0,0 +1,230 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { If } from "react-haiku";
+import UpdateQuestion from "./Modal/UpdateQuestion";
+import AdminViewQuestion from "./Modal/AdminViewQuestion";
+import ViewQuestion from "./Modal/ViewQuestion";
+import AdminViewAnsweredQuestion from "./Modal/AdminViewAnsweredQuestion";
+import ViewAnsweredQuestion from "./Modal/ViewAnsweredQuestion";
+import AnswerQuestion from "./Modal/AnswerQuestion";
+import useSWR from "swr"
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function DropDownQuestion({ item, permission, fullName }) {
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showAnswerModal, setShowAnswerModal] = useState(false);
+  const [showDetailModal1, setShowDetailModal1] = useState(false);
+  const [showDetailModal2, setShowDetailModal2] = useState(false);
+  const [showDetailModal3, setShowDetailModal3] = useState(false);
+  const [showDetailModal4, setShowDetailModal4] = useState(false);
+
+  const removeQuestion = async (number) => {
+    const endpoint = `http://localhost:8080/question/delete/${number}`;
+    const options = {
+      method: "DELETE",
+      redirect: "follow",
+    };
+    const response = await fetch(endpoint, options);
+    if (response.status === 204) {
+      alert("Xóa thành công");
+    } else {
+      const result = await response.json();
+      alert(result.message)
+    }
+  };
+  return (
+    <>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="flex items-center text-gray-400 rounded-full hover:text-gray-600 focus:outline-none"
+          >
+            <span className="sr-only">Open options</span>
+            <EllipsisVerticalIcon className="h-6 mt-1 w-7" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-20 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+            <div className="flex flex-col py-1">
+              <If isTrue={permission?.user.role !== "Trưởng họ" && item.status === "Đã phản hồi"}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowDetailModal3(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Xem
+                    </div>
+                  )}
+                </Menu.Item>
+              </If>
+              <If isTrue={permission?.user.role !== "Trưởng họ" && item.status !== "Đã phản hồi"}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowDetailModal4(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Xem
+                    </div>
+                  )}
+                </Menu.Item>
+              </If>
+              <If isTrue={permission?.user.role === "Trưởng họ" && item.status === "Đã phản hồi"}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowDetailModal1(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Xem
+                    </div>
+                  )}
+                </Menu.Item>
+              </If>
+              <If isTrue={permission?.user.role === "Trưởng họ" && item.status !== "Đã phản hồi"}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowDetailModal2(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Xem
+                    </div>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowAnswerModal(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Trả lời
+                    </div>
+                  )}
+                </Menu.Item>
+              </If>
+              <If isTrue={permission?.user.role !== "Trưởng họ"}>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        setShowUpdateModal(true);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Sửa
+                    </div>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <div
+                      onClick={() => {
+                        removeQuestion(item.id);
+                      }}
+                      className={classNames(
+                        active ? "bg-gray-300 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-base"
+                      )}
+                    >
+                      Xóa
+                    </div>
+                  )}
+                </Menu.Item>
+              </If>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+      <If isTrue={showUpdateModal}>
+        <UpdateQuestion
+          isVisible={showUpdateModal}
+          onClose={() => setShowUpdateModal(false)}
+          item={item}
+        />
+      </If>
+      <If isTrue={showDetailModal2}>
+        <AdminViewQuestion
+          isVisible={showDetailModal2}
+          onClose={() => setShowDetailModal2(false)}
+          item={item}
+        />
+      </If>
+      <If isTrue={showDetailModal1}>
+        <AdminViewAnsweredQuestion
+          isVisible={showDetailModal1}
+          onClose={() => setShowDetailModal1(false)}
+          item={item}
+          fullName={fullName}
+        />
+      </If>
+      <If isTrue={showDetailModal3}>
+        <ViewAnsweredQuestion
+          isVisible={showDetailModal3}
+          onClose={() => setShowDetailModal1(false)}
+          item={item}
+        />
+      </If>
+      <If isTrue={showDetailModal4}>
+        <ViewQuestion
+          isVisible={showDetailModal4}
+          onClose={() => setShowDetailModal1(false)}
+          item={item}
+        />
+      </If>
+      <If isTrue={showAnswerModal}>
+        <AnswerQuestion
+          isVisible={showAnswerModal}
+          onClose={() => setShowAnswerModal(false)}
+          item={item}
+        />
+      </If>
+    </>
+  );
+}
---
+++ treefamily/Frontend/components/QuestionsAndAnswers/QuestionTable.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { For, If } from "react-haiku";
+import useSWR from "swr";
+import QuestionItem from "./QuestionItem";
+
+export default function QuestionTable({ permission }) {
+  const isConditionalMet1 = permission?.user.role !== "Trưởng họ"
+  const isConditionalMet2 = permission?.user.role === "Trưởng họ"
+  const { data, error } = useSWR(
+    isConditionalMet2 ? "http://localhost:8080/question/get-all" : null
+  );
+  const { data: userData, error: userError } = useSWR(
+    isConditionalMet1 ? `http://localhost:8080/question/get-all-for-a-member/${permission?.user.memberId}` : null
+  )
+  if (!data && !userData) {
+    return <div className="flex flex-col mt-8 overflow-y-scroll h-80vh"></div>;
+  }
+
+  return (
+    <div className="flex flex-col mt-8 overflow-y-scroll h-80vh">
+      {/* <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8"> */}
+      <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8 ">
+        <div className="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg ">
+          <table className="min-w-full divide-y divide-gray-300 ">
+            <thead className="sticky top-0 z-20 rounded-t-lg bg-gray-50">
+              <tr className="">
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 sm:pl-6"
+                >
+                  STT
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-base font-semibold text-gray-900"
+                >
+                  Tiêu đề câu hỏi
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-base font-semibold text-gray-900"
+                >
+                  Ngày gửi
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-base font-semibold text-gray-900"
+                >
+                  Trạng thái
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 w-1/12 py-3.5 text-left text-base font-semibold text-gray-900"
+                >
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200 ">
+              <If isTrue={permission?.user.role === "Trưởng họ"}>
+                <For
+                  each={data}
+                  render={(item, index) => (
+                    <QuestionItem item={item} index={index} permission={permission} />
+                  )}
+                />
+              </If>
+              <If isTrue={permission?.user.role !== "Trưởng họ"}>
+                <For
+                  each={userData}
+                  render={(item, index) => (
+                    <QuestionItem item={item} index={index} permission={permission} />
+                  )}
+                />
+              </If>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
